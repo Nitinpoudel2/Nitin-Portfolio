174db3ae76ea17a4e584cd2a94cb91dfccd4bebb
Nitin Paudel <57403447+Nitinpoudel2@users.noreply.github.com>
2024-04-30 10:09:09 -0500

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -7,7 +7,7 @@ const Projects = () => {
     <h1 className='text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20'>
         My Projects
         </h1>
-        <div className='h-full w-full flex flex-col md:flex-row gap-10 px-10' style={{ width: "100%", height: "300px" }}>
+        <div className='h-full w-full flex flex-col md:flex-row gap-10 px-10'>
             <ProjectCard 
             src='./NextWebsite.png'
             title='Modern Next.js Portfolio'
