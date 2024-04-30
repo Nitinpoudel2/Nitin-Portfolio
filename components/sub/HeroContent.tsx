d76c5a18d8bc96a507a94b3fe6d9ec6269779307
Nitin Paudel <57403447+Nitinpoudel2@users.noreply.github.com>
2024-04-30 10:28:40 -0500

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -24,20 +24,16 @@ const HeroContent = () => {
             FullStack Developer Portfolio
           </h1>
         </motion.div>
-        <motion.div
+             <motion.div
           variants={slideInFromLeft(0.5)}
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
             Providing
-               <span>
-            Providing
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
               the best{" "}
             </span>
-            project experience
-          </span>
             project experience
           </span>
         </motion.div>
