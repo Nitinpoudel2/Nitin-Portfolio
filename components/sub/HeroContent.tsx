fa0e32e420104b06bedcb8037718d8630319b3a5
Nitin Paudel <57403447+Nitinpoudel2@users.noreply.github.com>
2024-04-30 10:23:22 -0500

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -30,10 +30,14 @@ const HeroContent = () => {
         >
           <span>
             Providing
+               <span>
+            Providing
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
               the best{" "}
             </span>
+            project experience
+          </span>
             project experience
           </span>
         </motion.div>
